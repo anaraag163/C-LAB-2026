a7f64786e063fe8114a961a44597fd548f31073d
anaraag163 <u26ai060@aid.svnit.ac.in>
2026-09-19 15:10:25 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,10 @@
+//  Question 3
+#include <stdio.h>
+int main() {
+    printf("Enter variable 1 and variable 2\n");
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int gross=(a+b);
+    printf("The gross salary is %d\n",gross);
+     return 0;
+}
